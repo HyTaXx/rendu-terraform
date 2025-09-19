0e62583766113af3e9a3dc7ab266912cff60434d
Clansou <clement.souplet78@gmail.com>
2025-09-19 15:21:25 +0200

    terraform

---
+++ terraform/provider.tf
@@ -0,0 +1,15 @@
+# Configuration provider simplifié
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.117.1"
+    }
+  }
+  required_version = ">= 1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
