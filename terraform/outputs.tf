7ef26008957258b0da173c4fe0ad4b58da40ba7b
Charles-Chrismann <charles.chrismann@free.fr>
2025-09-19 11:40:42 +0200

    fix eslint

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,15 +2,8 @@ output "cosmos_endpoint" {
   value = azurerm_cosmosdb_account.crypto_cosmos.endpoint
 }
 
-output "cosmos_primary_key" {
-  value     = data.azurerm_cosmosdb_account_keys.crypto_keys.primary_master_key
-  sensitive = true
-}
-
-output "cosmos_secondary_key" {
-  value     = data.azurerm_cosmosdb_account_keys.crypto_keys.secondary_master_key
-  sensitive = true
-}
+# On ne sort pas les clés pour ne pas écraser tes credentials existants
+# Tu continues d’utiliser les secrets GitHub pour COSMOS_KEY
 
 output "aks_cluster_name" {
   value = azurerm_kubernetes_cluster.crypto_aks.name
